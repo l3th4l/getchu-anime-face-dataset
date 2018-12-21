0b8e7baa8767a2b5753e7f37d3ec7c4c56f98d28
ntt123 <s140046@ntu.edu.sg>
2018-12-21 16:01:14 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,76 @@
+# Getchu Anime Face Dataset
+
+## Download Getchu dataset
+
+### Query SQL 
+
+Run the following query:
+
+```
+SELECT g.id, g.gamename, g.sellday,
+'www.getchu.com/soft.phtml?id=' || g.comike as links
+FROM gamelist g
+WHERE g.comike is NOT NULL
+ORDER BY g.sellday
+```
+
+at http://erogamescape.dyndns.org/~ap2/ero/toukei_kaiseki/sql_for_erogamer_form.php
+
+Otherwise, we can use `curl` to do the same thing:
+
+```
+curl 'http://erogamescape.dyndns.org/~ap2/ero/toukei_kaiseki/sql_for_erogamer_form.php' -H 'Connection: keep-alive' -H 'Cache-Control: max-age=0' -H 'Origin: http://erogamescape.dyndns.org' -H 'Upgrade-Insecure-Requests: 1' -H 'DNT: 1' -H 'Content-Type: application/x-www-form-urlencoded' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8' -H 'Referer: http://erogamescape.dyndns.org/~ap2/ero/toukei_kaiseki/sql_for_erogamer_form.php' -H 'Accept-Encoding: gzip, deflate' -H 'Accept-Language: vi,en-US;q=0.9,en;q=0.8' --data 'sql=SELECT+g.id%2C+g.gamename%2C+g.sellday%2C%0D%0A%27www.getchu.com%2Fsoft.phtml%3Fid%3D%27+%7C%7C+g.comike+as+links%0D%0AFROM+gamelist+g%0D%0AWHERE+g.comike+is+NOT+NULL%0D%0AORDER+BY+g.sellday%0D%0A' --compressed --output getchu.html
+```
+
+The output is stored at `getchu.html`
+
+### Extract Link and Date information
+
+```
+cat getchu.html | grep -Ehro "www.getchu.com/soft.phtml\?id=[0-9]+" > links.txt
+cat getchu.html | grep -Ehro         "[0-9]+-[0-9][0-9]-[0-9][0-9]" > dates.txt
+```
+
+Append some magic, to bypass the warning
+
+```
+awk '{print $0"&gc=gc"}' links.txt > links_.txt
+```
+
+### Create a new directory and download web pages 
+
+```
+mkdir getchu
+cd getchu
+cat ../links_.txt | parallel -j 32 wget -q -O {#}.html {}
+cd ..
+```
+
+### Extract image links
+
+```
+cat getchu/* | grep -Ehro "./brandnew/[0-9]+/c[0-9]+chara[0-9]+.jpg" > img_links.txt
+awk '{print "www.getchu.com/"substr($0,3)}' img_links.txt > img_links_.txt
+```
+
+### Download images
+
+```
+mkdir imgs
+cd imgs && cat ../img_links_.txt | parallel -j 32 wget -q -O {#}.jpg {}
+```
+
+
+## Extract Faces
+
+
+```
+find ./imgs | parallel -j 8 python detect.py {}
+```
+
+
+List of all faces
+
+```
+find ./imgs | grep cropped > faces.txt
+```
---
+++ detect.py
@@ -0,0 +1,101 @@
+import animeface
+import PIL.Image
+
+import cv2
+import sys
+import os.path
+import math
+import numpy as np 
+
+import glob
+files = glob.glob('imgs/*.jpg', recursive=True)
+
+def crop_face(filename):
+    im = PIL.Image.open(filename)
+
+
+    faces = animeface.detect(im)
+
+    if len(faces) != 1:
+        return None
+
+    image = cv2.imread(filename, cv2.IMREAD_COLOR)
+
+    def imcrop(img, bbox): 
+        x1,y1,x2,y2 = bbox
+        if x1 < 0 or y1 < 0 or x2 > img.shape[1] or y2 > img.shape[0]:
+            img, x1, x2, y1, y2 = pad_img_to_fit_bbox(img, x1, x2, y1, y2)
+        return img[y1:y2, x1:x2, :]
+
+    def pad_img_to_fit_bbox(img, x1, x2, y1, y2):
+        img = np.pad(img, ((np.abs(np.minimum(0, y1)), np.maximum(y2 - img.shape[0], 0)),
+                   (np.abs(np.minimum(0, x1)), np.maximum(x2 - img.shape[1], 0)), (0,0)), mode="constant", constant_values=255)
+        y1 += np.abs(np.minimum(0, y1))
+        y2 += np.abs(np.minimum(0, y1))
+        x1 += np.abs(np.minimum(0, x1))
+        x2 += np.abs(np.minimum(0, x1))
+        return img, x1, x2, y1, y2
+
+
+
+
+    for face in faces:
+        L_x = face.left_eye.pos.x + face.left_eye.pos.height//2
+        L_y = face.left_eye.pos.y + face.left_eye.pos.width//2 
+        R_x = face.right_eye.pos.x + face.right_eye.pos.height//2
+        R_y = face.right_eye.pos.y + face.right_eye.pos.width//2
+
+        dx = L_x - R_x
+        dy = L_y - R_y
+
+        deg = math.atan(dy/dx) * 360 / (2*math.pi)
+        if math.fabs(deg) > 60:
+            deg = 0.0           # something wrong???
+
+        print(dx, dy, deg)
+
+        x = face.face.pos.x
+        y = face.face.pos.y
+        w = face.face.pos.width
+        h = face.face.pos.height
+
+        cx = x  +  h//2
+        cy = y  +  w//2
+
+        image = np.pad(image, ((2*x,2*x), (2*y,2*y), (0,0)), mode="constant", constant_values=255)
+        rows,cols,_ = image.shape
+
+        M = cv2.getRotationMatrix2D((0/2,0/2),deg,1)
+        image = cv2.warpAffine(image,M,(cols,rows), borderValue=(255,255,255), flags=cv2.INTER_CUBIC)
+
+        faces = animeface.detect(PIL.Image.fromarray(image))
+        if len(faces) != 1:
+            continue
+
+        face = faces[0]
+
+        x = face.face.pos.x
+        y = face.face.pos.y
+        w = face.face.pos.width + 10
+        h = face.face.pos.height + 10
+
+        chinx = face.chin.pos.x
+        chiny = face.chin.pos.y
+
+        cx = x  +  h//2
+        cy = y  +  w//2
+
+        d = h 
+
+        cy = chiny - d + d//3
+
+        if cy - d < 0:
+            cy -= (cy-d)//2
+
+        image = imcrop(image, (cx - d, cy - d, cx+d, cy+d ) )
+
+        cv2.imwrite("{}_cropped.jpg".format(filename), image)
+
+
+
+crop_face(sys.argv[1])
